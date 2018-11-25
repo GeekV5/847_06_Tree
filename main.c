a242577e00bf5a2b0b6f7be93f368358503d2d7c
PhuPing <hfping@aliyun.com>
2018-11-25 23:45:18 +0800

    Update

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
+//第 6 章 树
 #include <stdio.h>
+
+//链式存储结构
+typedef struct BTNode
+{
+    char data;     //这里默认结点data域为char型，如果题目中需要其他类型，则只需修改此处
+    struct BTNode *lchild;
+    struct BTNode *rchild;
+}BTNode;
 
 int main() {
     printf("Hello, World!\n");
