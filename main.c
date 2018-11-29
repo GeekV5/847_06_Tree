0679f4dc2f87e922faab8dd1cf645899a9a97d32
PhuPing <hfping@aliyun.com>
2018-11-29 22:30:19 +0800

    Update

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 //第 6 章 树
 #include <stdio.h>
+#include <stdlib.h>
 
 //链式存储结构
 typedef struct BTNode
@@ -15,7 +16,46 @@ void Visit(BTNode *p)
     printf("%c",p->data);
 }
 
+//先序遍历创建二叉树
+void CreateBiTree(BTNode *bt)
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch == ' ')
+    {
+        bt = NULL;
+    }
+    if(ch == '#')
+    {
+        return;
+    }
+    else{
+        bt = (BTNode*)malloc(sizeof(BTNode));
+        if(!(bt))
+        {
+            exit(-1);
+        }
+        bt->data = ch;
+        CreateBiTree(&bt->lchild);
+        CreateBiTree(&bt->rchild);
+    }
+}
+
+//先序遍历二叉树
+void PreOrder(BTNode *bt)
+{
+    if(bt != NULL)
+    {
+        Visit(bt);
+        PreOrder(bt->lchild);
+        PreOrder(bt->rchild);
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printf("欢迎来到二叉树世界!\n");
+    BTNode *bitreenode;
+    CreateBiTree(&bitreenode);
+    PreOrder(bitreenode);
     return 0;
 }
